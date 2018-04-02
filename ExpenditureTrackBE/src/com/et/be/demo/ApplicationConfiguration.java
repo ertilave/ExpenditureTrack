091ce1d33ea066a42aec24e60537361ef85c0c2e
ertilave <engin.ertilav@gmail.com>
2018-04-03 01:19:29 +0300

    Rest BE Design

---
+++ ExpenditureTrackBE/src/com/et/be/demo/ApplicationConfiguration.java
@@ -0,0 +1,12 @@
+package com.et.be.demo;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.et.be.demo")
+public class ApplicationConfiguration {
+
+}
